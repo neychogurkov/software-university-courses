89812e75543397f310c3e2dbbf8ba1b89ab5625c
Neycho Gurkov <nglolplayer@gmail.com>
2022-06-10 22:48:42 +0300

    Made some changes to Array Manipulator problem

--- CSharp-Fundamentals-May-2022/Labs-And-Exercises/04.MethodsExercise/11.ArrayManipulator/Program.cs
+++ CSharp-Fundamentals-May-2022/Labs-And-Exercises/04.MethodsExercise/11.ArrayManipulator/Program.cs
@@ -9,15 +9,15 @@ namespace _11.ArrayManipulator
         static void Main(string[] args)
         {
             int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            string command = Console.ReadLine();
+            string input = Console.ReadLine();
 
-            while (command != "end")
+            while (input != "end")
             {
-                string[] elements = command.Split();
+                string[] command = input.Split();
 
-                if (elements[0] == "exchange")
+                if (command[0] == "exchange")
                 {
-                    int index = int.Parse(elements[1]);
+                    int index = int.Parse(command[1]);
 
                     if (index < 0 || index >= numbers.Length)
                     {
@@ -25,220 +25,186 @@ namespace _11.ArrayManipulator
                     }
                     else
                     {
-                        ExchangeArray(numbers, index);
+                        numbers = ExchangeArray(numbers, index);
                     }
                 }
-                else if (elements[0] == "max")
+                else if (command[0] == "max")
                 {
-                    if (elements[1] == "even")
+                    if (command[1] == "even")
                     {
                         PrintIndexOfMaxEvenElement(numbers);
                     }
-                    else if (elements[1] == "odd")
+                    else if (command[1] == "odd")
                     {
                         PrintIndexOfMaxOddElement(numbers);
                     }
                 }
-                else if (elements[0] == "min")
+                else if (command[0] == "min")
                 {
-                    if (elements[1] == "even")
+                    if (command[1] == "even")
                     {
                         PrintIndexOfMinEvenElement(numbers);
                     }
-                    else if (elements[1] == "odd")
+                    else if (command[1] == "odd")
                     {
                         PrintIndexOfMinOddElement(numbers);
                     }
                 }
-                else if (elements[0] == "first")
+                else if (command[0] == "first")
                 {
-                    int count = int.Parse(elements[1]);
+                    int count = int.Parse(command[1]);
                     if (count > numbers.Length)
                     {
                         Console.WriteLine("Invalid count");
                     }
                     else
                     {
-                        if (elements[2] == "even")
+                        if (command[2] == "even")
                         {
                             GetFirstCountEvenElements(numbers, count);
                         }
-                        else if (elements[2] == "odd")
+                        else if (command[2] == "odd")
                         {
                             GetFirstCountOddElements(numbers, count);
                         }
                     }
                 }
-                else if (elements[0] == "last")
+                else if (command[0] == "last")
                 {
-                    int count = int.Parse(elements[1]);
+                    int count = int.Parse(command[1]);
                     if (count > numbers.Length)
                     {
                         Console.WriteLine("Invalid count");
                     }
                     else
                     {
-                        if (elements[2] == "even")
+                        if (command[2] == "even")
                         {
                             GetLastCountEvenElements(numbers, count);
                         }
-                        else if (elements[2] == "odd")
+                        else if (command[2] == "odd")
                         {
                             GetLastCountOddElements(numbers, count);
                         }
                     }
                 }
 
-                command = Console.ReadLine();
+                input = Console.ReadLine();
             }
 
             Console.WriteLine($"[{string.Join(", ", numbers)}]");
         }
 
-        static void ExchangeArray(int[] array, int index)
+        static int[] ExchangeArray(int[] array, int splitIndex)
         {
-            int[] subArray1 = new int[array.Length - index - 1];
-            int[] subArray2 = new int[index + 1];
+            int[] exchangedArray = new int[array.Length];
+            int index = 0;
 
-            for (int i = 0; i < subArray1.Length; i++)
+            for (int i = splitIndex + 1; i < array.Length; i++)
             {
-                subArray1[i] = array[i + index + 1];
+                exchangedArray[index] = array[i];
+                index++;
             }
 
-            for (int i = 0; i < subArray2.Length; i++)
+            for (int i = 0; i <= splitIndex; i++)
             {
-                subArray2[i] = array[i];
+                exchangedArray[index] = array[i];
+                index++;
             }
 
-            for (int i = 0; i < subArray1.Length; i++)
-            {
-                array[i] = subArray1[i];
-            }
-
-            for (int i = 0; i < subArray2.Length; i++)
-            {
-                array[subArray1.Length + i] = subArray2[i];
-            }
+            return exchangedArray;
         }
 
         static void PrintIndexOfMaxEvenElement(int[] array)
         {
-            int max = int.MinValue;
+            int maxEvenNumber = int.MinValue;
             int maxIndex = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 == 0)
                 {
-                    if (array[i] >= max)
+                    if (array[i] >= maxEvenNumber)
                     {
-                        max = array[i];
+                        maxEvenNumber = array[i];
                         maxIndex = i;
                     }
                 }
             }
 
-            if (max == int.MinValue)
-            {
-                Console.WriteLine("No matches");
-            }
-            else
-            {
-                Console.WriteLine(maxIndex);
-            }
+            Console.WriteLine(maxEvenNumber == int.MinValue ? "No matches" : maxIndex.ToString());
         }
 
         static void PrintIndexOfMaxOddElement(int[] array)
         {
-            int max = int.MinValue;
+            int maxOddNumber = int.MinValue;
             int maxIndex = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 != 0)
                 {
-                    if (array[i] >= max)
+                    if (array[i] >= maxOddNumber)
                     {
-                        max = array[i];
+                        maxOddNumber = array[i];
                         maxIndex = i;
                     }
                 }
             }
 
-            if (max == int.MinValue)
-            {
-                Console.WriteLine("No matches");
-            }
-            else
-            {
-                Console.WriteLine(maxIndex);
-            }
+            Console.WriteLine(maxOddNumber == int.MinValue ? "No matches" : maxIndex.ToString());
         }
 
         static void PrintIndexOfMinEvenElement(int[] array)
         {
-            int min = int.MaxValue;
+            int minOddNumber = int.MaxValue;
             int minIndex = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 == 0)
                 {
-                    if (array[i] <= min)
+                    if (array[i] <= minOddNumber)
                     {
-                        min = array[i];
+                        minOddNumber = array[i];
                         minIndex = i;
                     }
                 }
             }
 
-            if (min == int.MaxValue)
-            {
-                Console.WriteLine("No matches");
-            }
-            else
-            {
-                Console.WriteLine(minIndex);
-            }
+            Console.WriteLine(minOddNumber == int.MaxValue ? "No matches" : minIndex.ToString());
         }
 
         static void PrintIndexOfMinOddElement(int[] array)
         {
-            int min = int.MaxValue;
+            int minOddNumber = int.MaxValue;
             int minIndex = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 != 0)
                 {
-                    if (array[i] <= min)
+                    if (array[i] <= minOddNumber)
                     {
-                        min = array[i];
+                        minOddNumber = array[i];
                         minIndex = i;
                     }
                 }
             }
 
-            if (min == int.MaxValue)
-            {
-                Console.WriteLine("No matches");
-            }
-            else
-            {
-                Console.WriteLine(minIndex);
-            }
+            Console.WriteLine(minOddNumber == int.MaxValue ? "No matches" : minIndex.ToString());
         }
 
         static void GetFirstCountEvenElements(int[] array, int count)
         {
-            List<int> evenElements = new List<int>();
+            List<int> evenNumbers = new List<int>();
             int evenCount = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 == 0)
                 {
-                    evenElements.Add(array[i]);
+                    evenNumbers.Add(array[i]);
                     evenCount++;
                 }
 
@@ -247,20 +213,20 @@ namespace _11.ArrayManipulator
                     break;
                 }
             }
-            
-            Console.WriteLine($"[{string.Join(", ", evenElements)}]");
+
+            Console.WriteLine($"[{string.Join(", ", evenNumbers)}]");
         }
 
         static void GetFirstCountOddElements(int[] array, int count)
         {
-            List<int> oddElements = new List<int>();
+            List<int> oddNumbers = new List<int>();
             int oddCount = 0;
 
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 != 0)
                 {
-                    oddElements.Add(array[i]);
+                    oddNumbers.Add(array[i]);
                     oddCount++;
                 }
 
@@ -270,19 +236,19 @@ namespace _11.ArrayManipulator
                 }
             }
 
-            Console.WriteLine($"[{string.Join(", ", oddElements)}]");
+            Console.WriteLine($"[{string.Join(", ", oddNumbers)}]");
         }
 
         static void GetLastCountEvenElements(int[] array, int count)
         {
-            List<int> evenElements = new List<int>();
+            List<int> evenNumbers = new List<int>();
             int evenCount = 0;
 
             for (int i = array.Length - 1; i >= 0; i--)
             {
                 if (array[i] % 2 == 0)
                 {
-                    evenElements.Add(array[i]);
+                    evenNumbers.Add(array[i]);
                     evenCount++;
                 }
 
@@ -292,20 +258,20 @@ namespace _11.ArrayManipulator
                 }
             }
 
-            evenElements.Reverse();
-            Console.WriteLine($"[{string.Join(", ", evenElements)}]");
+            evenNumbers.Reverse();
+            Console.WriteLine($"[{string.Join(", ", evenNumbers)}]");
         }
 
         static void GetLastCountOddElements(int[] array, int count)
         {
-            List<int> oddElements = new List<int>();
+            List<int> oddNumbers = new List<int>();
             int oddCount = 0;
 
             for (int i = array.Length - 1; i >= 0; i--)
             {
                 if (array[i] % 2 != 0)
                 {
-                    oddElements.Add(array[i]);
+                    oddNumbers.Add(array[i]);
                     oddCount++;
                 }
 
@@ -315,8 +281,8 @@ namespace _11.ArrayManipulator
                 }
             }
 
-            oddElements.Reverse();
-            Console.WriteLine($"[{string.Join(", ", oddElements)}]");
+            oddNumbers.Reverse();
+            Console.WriteLine($"[{string.Join(", ", oddNumbers)}]");
         }
     }
 }
