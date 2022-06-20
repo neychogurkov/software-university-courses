a9b93f88f455db326bcd87c632d4f1bc711fe09c
Neycho Gurkov <nglolplayer@gmail.com>
2022-06-20 14:52:07 +0300

    Added MidExamPreparation

---
+++ CSharp-Fundamentals-May-2022/Labs-And-Exercises/06.MidExamPreparation/02.TheLift/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int waitingPeople = int.Parse(Console.ReadLine());
+            int[] wagons = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool isFull = true;
+
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                if (wagons[i] < 4)
+                {
+                    int peopleToJoin = Math.Min(Math.Min(waitingPeople, 4), 4 - wagons[i]);
+                    wagons[i] += peopleToJoin;
+                    waitingPeople -= peopleToJoin;
+                }
+            }
+
+            if (wagons[wagons.Length - 1] < 4)
+            {
+                isFull = false;
+            }
+
+            if (waitingPeople == 0 && isFull == false)
+            {
+                Console.WriteLine("The lift has empty spots!");
+            }
+            else if (isFull && waitingPeople > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {waitingPeople} people in a queue!");
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ CSharp-Fundamentals-May-2022/Labs-And-Exercises/06.MidExamPreparation/03.MovingTarget/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MovingTarget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                int index = int.Parse(tokens[1]);
+
+                switch (command)
+                {
+                    case "Shoot":
+                        int power = int.Parse(tokens[2]);
+
+                        if (index >= 0 && index < targets.Count)
+                        {
+                            targets[index] -= power;
+
+                            if (targets[index] <= 0)
+                            {
+                                targets.RemoveAt(index);
+                            }
+                        }
+                        break;
+                    case "Add":
+                        int value = int.Parse(tokens[2]);
+
+                        if (index >= 0 && index < targets.Count)
+                        {
+                            targets.Insert(index, value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid placement!");
+                        }
+                        break;
+                    case "Strike":
+                        int radius = int.Parse(tokens[2]);
+
+                        if (index - radius >= 0 && index - radius < targets.Count &&
+                            index + radius >= 0 && index + radius < targets.Count &&
+                            index >= 0 && index < targets.Count)
+                        {
+                            targets.RemoveRange(index - radius, radius * 2 + 1);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Strike missed!");
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join('|', targets));
+        }
+    }
+}
