a70e751524b61b549a2d60bfa8fa8ef7b60c2a49
Neycho Gurkov <nglolplayer@gmail.com>
2022-07-20 21:34:38 +0300

    Added RegularExpressionsLab

---
+++ CSharp-Fundamentals-May-2022/Labs-And-Exercises/09.RegularExpressionsLab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phoneNumbers = Console.ReadLine();
+
+            string pattern = @"\+359([- ])2\1[0-9]{3}\1[0-9]{4}\b";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection validPhoneNumbers = regex.Matches(phoneNumbers);
+
+            Console.WriteLine(string.Join(", ", validPhoneNumbers));
+        }
+    }
+}
---
+++ CSharp-Fundamentals-May-2022/Labs-And-Exercises/09.RegularExpressionsLab/03.MatchDates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dates = Console.ReadLine();
+
+            string pattern = @"(?<day>\d{2})(?<separator>[-\/.])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>\d{4})";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection validDates = regex.Matches(dates);
+
+            foreach (Match validDate in validDates)
+            {
+                Console.WriteLine($"Day: {validDate.Groups["day"]}, Month: {validDate.Groups["month"]}, Year: {validDate.Groups["year"]}");
+            }
+        }
+    }
+}
