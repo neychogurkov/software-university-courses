6c18944848ffa38628744381db123533c67910de
Neycho Gurkov <nglolplayer@gmail.com>
2022-10-04 23:23:48 +0300

    Added ImplementingLinkedList

---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        public ListNode Head { get; set; }
+        public ListNode Tail { get; set; }
+        public int Count { get; set; }
+
+        public void AddFirst(int element)
+        {
+            this.Count++;
+            ListNode node = new ListNode(element);
+
+            if (this.Head == null)
+            {
+                this.Head = node;
+                this.Tail = node;
+            }
+            else
+            {
+                ListNode oldHead = this.Head;
+                this.Head.PreviousNode = node;
+                this.Head = node;
+                this.Head.NextNode = oldHead;
+            }
+        }
+
+        public void AddLast(int element)
+        {
+            this.Count++;
+            ListNode node = new ListNode(element);
+
+            if (this.Head == null)
+            {
+                this.Head = node;
+                this.Tail = node;
+            }
+            else
+            {
+                ListNode oldTail = this.Tail;
+                this.Tail.NextNode = node;
+                this.Tail = node;
+                this.Tail.PreviousNode = oldTail;
+            }
+        }
+
+        public int RemoveFirst()
+        {
+            if (this.Head == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            int first = this.Head.Value;
+
+            if (this.Head == this.Tail)
+            {
+                this.Head = null;
+                this.Tail = null;
+            }
+            else
+            {
+                this.Head = this.Head.NextNode;
+                this.Head.PreviousNode = null;
+            }
+
+            this.Count--;
+            return first;
+        }
+
+        public int RemoveLast()
+        {
+            if (this.Tail == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            int last = this.Tail.Value;
+
+            if (this.Tail == this.Head)
+            {
+                this.Head = null;
+                this.Tail = null;
+            }
+            else
+            {
+                this.Tail = this.Tail.PreviousNode;
+                this.Tail.NextNode = null;
+            }
+
+            this.Count--;
+            return last;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            ListNode node = this.Head;
+
+            while (node != null)
+            {
+                action(node.Value);
+                node = node.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[this.Count];
+
+            ListNode node = this.Head;
+            int index = 0;
+
+            while (node != null)
+            {
+                array[index++] = node.Value;
+                node = node.NextNode;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingLinkedList/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            doublyLinkedList.AddFirst(3);
+            doublyLinkedList.AddFirst(2);
+            doublyLinkedList.AddLast(5);
+            doublyLinkedList.AddFirst(4);
+            doublyLinkedList.AddLast(6);
+            doublyLinkedList.AddFirst(7);
+
+            // 7 4 2 3 5 6
+            Console.WriteLine(doublyLinkedList.RemoveLast()); // 6
+            Console.WriteLine(doublyLinkedList.RemoveLast()); // 5
+            Console.WriteLine(doublyLinkedList.RemoveFirst()); // 7
+            Console.WriteLine(doublyLinkedList.RemoveLast()); // 3
+            Console.WriteLine(doublyLinkedList.RemoveFirst()); // 4
+            Console.WriteLine(doublyLinkedList.RemoveLast()); // 2
+
+            doublyLinkedList.AddFirst(12);
+            doublyLinkedList.AddLast(2);
+            doublyLinkedList.AddLast(5);
+            doublyLinkedList.AddFirst(8);
+            doublyLinkedList.AddLast(1);
+            doublyLinkedList.AddFirst(3);
+
+            // 3 8 12 2 5 1 
+
+            doublyLinkedList.ForEach(x => Console.WriteLine(x));
+
+            doublyLinkedList.RemoveLast();
+            doublyLinkedList.RemoveFirst();
+
+            Console.WriteLine(string.Join(' ', doublyLinkedList.ToArray()));
+        }
+    }
+}
