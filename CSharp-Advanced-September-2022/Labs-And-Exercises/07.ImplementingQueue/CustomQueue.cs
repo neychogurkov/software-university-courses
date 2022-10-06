533dbd499febe04f3fa9ae63887b7e5ed86f1fa0
Neycho Gurkov <nglolplayer@gmail.com>
2022-10-06 20:59:17 +0300

    Added ImplementingQueue

---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingQueue/CustomQueue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ImplementingQueue
+{
+    public class CustomQueue
+    {
+        private const int InitialCapacity = 4;
+        private const int FirstElementIndex = 0;
+        private int[] items;
+
+        public CustomQueue()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Resize()
+        {
+            int[] internalArray = new int[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                internalArray[i] = this.items[i];
+            }
+
+            this.items = internalArray;
+        }
+
+        private void ShiftLeft()
+        {
+            for (int i = 0; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+                this.items[i + 1] = default(int);
+            }
+        }
+
+        public void Enqueue(int element)
+        {
+            if (this.items.Length == this.Count)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count] = element;
+            this.Count++;
+        }
+
+        public int Dequeue()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty!");
+            }
+
+            int item = this.items[FirstElementIndex];
+            this.ShiftLeft();
+            this.Count--;
+
+            return item;
+        }
+
+        public int Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty!");
+            }
+
+            return this.items[FirstElementIndex];
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                this.items[i] = default(int);
+            }
+
+            this.Count = 0;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingQueue/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ImplementingQueue
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue queue = new CustomQueue();
+
+            queue.Enqueue(5);
+            queue.Enqueue(3);
+            queue.Enqueue(7);
+            queue.Enqueue(2);
+            queue.Enqueue(8);
+
+            Console.Write("Queue:");
+            queue.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Peek());
+
+            Console.Write("Queue:");
+            queue.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            Console.WriteLine($"Count: {queue.Count}");
+            queue.Clear();
+            Console.WriteLine($"Count: {queue.Count}");
+
+        }
+    }
+}
