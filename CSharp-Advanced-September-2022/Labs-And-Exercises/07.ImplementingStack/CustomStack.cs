1700287deb4a3f26eb35ece966a5feae378a57ee
Neycho Gurkov <nglolplayer@gmail.com>
2022-10-06 20:29:03 +0300

    Added ImplementingStack

---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingStack/CustomStack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ImplementingStack
+{
+    public class CustomStack
+    {
+        private const int InitialCapacity = 4;
+        private int[] items;
+
+        public CustomStack()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Resize()
+        {
+            int[] internalArray = new int[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                internalArray[i] = this.items[i];
+            }
+
+            this.items = internalArray;
+        }
+
+        public void Push(int element)
+        {
+            if (this.items.Length == this.Count)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count] = element;
+            this.Count++;
+        }
+
+        public int Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty!");
+            }
+
+            int item = this.items[this.Count - 1];
+            this.items[this.Count - 1] = default(int);
+            this.Count--;
+
+            return item;
+        }
+
+        public int Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty!");
+            }
+
+            return this.items[this.Count - 1];
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                action(this.items[i]);
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingStack/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImplementingStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(7);
+            stack.Push(9);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+
+            stack.Push(1); 
+            stack.Push(6);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Count);
+
+            stack.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
