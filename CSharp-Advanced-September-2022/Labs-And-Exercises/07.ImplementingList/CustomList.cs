e4d9d29642b907f07a185710e269a0b8bd8ee6c6
Neycho Gurkov <nglolplayer@gmail.com>
2022-10-06 19:42:24 +0300

    Added ImplementingList

---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingList/CustomList.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Text;
+
+namespace ImplementingList
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+
+        public CustomList()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                items[index] = value;
+            }
+        }
+
+        private void Resize()
+        {
+            int[] internalArray = new int[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                internalArray[i] = this.items[i];
+            }
+
+            this.items = internalArray;
+        }
+
+        public void Shrink()
+        {
+            int[] internalArray = new int[this.items.Length / 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                internalArray[i] = this.items[i];
+            }
+
+            this.items = internalArray;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+                this.items[i + 1] = 0;
+            }
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = this.Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+        }
+
+        public void Add(int element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count] = element;
+            this.Count++;
+        }
+
+        public void AddRange(int[] range)
+        {
+            foreach (var item in range)
+            {
+                this.Add(item);
+            }
+        }
+
+        public int RemoveAt(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int item = this.items[index];
+            this.items[index] = default(int);
+            this.ShiftLeft(index);
+
+            this.Count--;
+            if (this.Count * 4 <= this.items.Length)
+            {
+                this.Shrink();
+            }
+
+            return item;
+        }
+
+        public void Remove(int element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.items[i] == element)
+                {
+                    this.items[i] = default(int);
+                    this.ShiftLeft(i);
+                    this.Count--;
+                    return;
+                }
+            }
+        }
+
+        public void RemoveAll(Predicate<int> predicate)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (predicate(this.items[i]))
+                {
+                    this.items[i] = default(int);
+                    this.ShiftLeft(i);
+                    this.Count--;
+                    i--;
+                }
+            }
+        }
+
+        public void Insert(int index, int element)
+        {
+            if (index < 0 || index >= this.Count && this.Count > 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.ShiftRight(index);
+            this.items[index] = element;
+            this.Count++;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.items[i] == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (firstIndex < 0 || firstIndex >= this.Count || secondIndex < 0 || secondIndex >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            (this.items[firstIndex], this.items[secondIndex]) = (this.items[secondIndex], this.items[firstIndex]);
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[this.Count];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                array[i] = this.items[i];
+            }
+
+            return array;
+        }
+
+        public int Find(Predicate<int> predicate)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (predicate(this.items[i]))
+                {
+                    return this.items[i];
+                }
+            }
+
+            return 0;
+        }
+
+        public CustomList FindAll(Predicate<int> predicate)
+        {
+            CustomList list = new CustomList();
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (predicate(this.items[i]))
+                {
+                    list.Add(this.items[i]);
+                }
+            }
+
+            return list;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+
+        public void Reverse()
+        {
+            for (int i = 0; i < this.Count / 2; i++)
+            {
+                (this.items[i], this.items[this.Count - 1 - i]) = (this.items[this.Count - 1 - i], this.items[i]);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                sb.Append(this.items[i] + " ");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/07.ImplementingList/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ImplementingList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            list.Add(3);
+            list.Add(5);
+            list.Add(6);
+
+            Console.WriteLine(list[0]);
+
+            (list[0], list[2]) = (list[2], list[0]);
+            Console.WriteLine(list[0]);
+
+            list.Add(7);
+            list.Add(2);
+            list.RemoveAt(2);
+            list.Insert(0, 3);
+            Console.WriteLine(list.Contains(7));
+            Console.WriteLine(list.Contains(15));
+            list.Swap(1, 4);
+            Console.WriteLine(list);
+
+            int[] array = list.ToArray();
+            Console.WriteLine(String.Join(Environment.NewLine, array));
+
+            list.Add(1);
+            Console.WriteLine(list.Find(x => x % 2 == 0));
+            list.Add(5);
+            list.Add(2);
+            Console.WriteLine(list.Find(x => x % 2 == 0));
+            list.Insert(0, 4);
+            Console.WriteLine(list.Find(x => x % 2 == 0));
+            Console.WriteLine(list);
+
+            list.Reverse();
+            Console.WriteLine(list);
+
+            list.AddRange(new int[] { 3, 54, 23, 21, 3, 213, 21, 34 });
+            Console.WriteLine(list);
+
+            list.Remove(54);
+            Console.WriteLine(list);
+
+            list.Remove(21);
+            Console.WriteLine(list);
+
+            list.FindAll(x => x % 2 != 0).ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            list.RemoveAll(x => x % 2 != 0);
+            Console.WriteLine(list);
+        }
+    }
+}
