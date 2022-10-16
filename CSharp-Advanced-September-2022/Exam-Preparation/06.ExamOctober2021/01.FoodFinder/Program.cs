baa36b9d77960445ee9b61aeb33e76f585b9ad03
Neycho Gurkov <nglolplayer@gmail.com>
2022-10-16 18:06:44 +0300

    Added ExamOctober2021

---
+++ CSharp-Advanced-September-2022/Exam-Preparation/06.ExamOctober2021/01.FoodFinder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.FoodFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> vowels = new Queue<char>(Console.ReadLine().Split().Select(char.Parse));
+            Stack<char> consonants = new Stack<char>(Console.ReadLine().Split().Select(char.Parse));
+
+            List<string> words = new List<string> { "pear", "flour", "pork", "olive" };
+            HashSet<char> letters = new HashSet<char>();
+
+            while (consonants.Any())
+            {
+                char currentVowel = vowels.Dequeue();
+                char currentConsonant = consonants.Pop();
+                vowels.Enqueue(currentVowel);
+
+                if (words.Any(w => w.Contains(currentVowel)))
+                {
+                    letters.Add(currentVowel);
+                }
+                if (words.Any(w => w.Contains(currentConsonant)))
+                {
+                    letters.Add(currentConsonant);
+                }
+            }
+
+            List<string> wordsFound = new List<string>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string word = words[i];
+
+                foreach (var letter in letters)
+                {
+                    if (word.Contains(letter))
+                    {
+                        word = word.Remove(word.IndexOf(letter), 1);
+                    }
+                }
+
+                if (word == string.Empty)
+                {
+                    wordsFound.Add(words[i]);
+                }
+            }
+
+            Console.WriteLine($"Words found: {wordsFound.Count}");
+
+            foreach (var word in wordsFound)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced-September-2022/Exam-Preparation/06.ExamOctober2021/02.PawnWars/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace _02.PawnWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int BoardSize = 8;
+
+            char[,] board = GetBoardData(BoardSize);
+            (int whitePawnRow, int whitePawnCol) = GetPawnPosition(board, BoardSize, 'w');
+            (int blackPawnRow, int blackPawnCol) = GetPawnPosition(board, BoardSize, 'b');
+
+            int turnCounter = 0;
+
+            while (true)
+            {
+                if (turnCounter % 2 == 0)
+                {
+                    if ((whitePawnCol > 0 && board[whitePawnRow - 1, whitePawnCol - 1] == 'b') || (whitePawnCol < BoardSize - 1 && board[whitePawnRow - 1, whitePawnCol + 1] == 'b'))
+                    {
+                        Console.WriteLine($"Game over! White capture on {(char)(blackPawnCol + 'a')}{BoardSize - blackPawnRow}.");
+                        return;
+                    }
+                    else
+                    {
+                        board[whitePawnRow--, whitePawnCol] = '-';
+                        board[whitePawnRow, whitePawnCol] = 'w';
+                    }
+
+                    if (whitePawnRow == 0)
+                    {
+                        Console.WriteLine($"Game over! White pawn is promoted to a queen at {(char)(whitePawnCol + 'a')}{BoardSize - whitePawnRow}.");
+                        return;
+                    }
+                }
+                else
+                {
+                    if ((blackPawnCol > 0 && board[blackPawnRow + 1, blackPawnCol - 1] == 'w') || (blackPawnCol < BoardSize - 1 && board[blackPawnRow + 1, blackPawnCol + 1] == 'w'))
+                    {
+                        Console.WriteLine($"Game over! Black capture on {(char)(whitePawnCol + 'a')}{BoardSize - whitePawnRow}.");
+                        return;
+                    }
+                    else
+                    {
+                        board[blackPawnRow++, blackPawnCol] = '-';
+                        board[blackPawnRow, blackPawnCol] = 'b';
+                    }
+
+                    if (blackPawnRow == BoardSize - 1)
+                    {
+                        Console.WriteLine($"Game over! Black pawn is promoted to a queen at {(char)(blackPawnCol + 'a')}{BoardSize - blackPawnRow}.");
+                        return;
+                    }
+                }
+
+                turnCounter++;
+            }
+        }
+
+        static char[,] GetBoardData(int size)
+        {
+            char[,] board = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string line = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    board[row, col] = line[col];
+                }
+            }
+
+            return board;
+        }
+
+        static (int, int) GetPawnPosition(char[,] board, int size, char color)
+        {
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (board[row, col] == color)
+                    {
+                        return (row, col);
+                    }
+                }
+            }
+
+            return (0, 0);
+        }
+    }
+}
