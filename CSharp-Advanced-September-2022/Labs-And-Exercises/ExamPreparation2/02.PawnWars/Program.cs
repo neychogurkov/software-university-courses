ba968dd76bab6e82f01638f067b40d70c510f8ec
Neycho Gurkov <nglolplayer@gmail.com>
2022-10-17 19:28:57 +0300

    Added ExamPreparation2

---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/ExamPreparation2/02.PawnWars/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _02.PawnWars
+{
+    internal class Program
+    {
+        private const int BoardSize = 8;
+
+        static void Main(string[] args)
+        {
+
+            char[,] board = GetBoardData();
+            (int whitePawnRow, int whitePawnCol) = GetPawnPosition(board, 'w');
+            (int blackPawnRow, int blackPawnCol) = GetPawnPosition(board, 'b');
+
+            int turnCounter = 0;
+
+            while (true)
+            {
+                string result = MovePawns(board, ref whitePawnRow, whitePawnCol, ref blackPawnRow, blackPawnCol, turnCounter);
+
+                if (result != null)
+                {
+                    Console.WriteLine(result);
+                    return;
+                }
+
+                turnCounter++;
+            }
+        }
+
+        static string MovePawns(char[,] board, ref int whitePawnRow, int whitePawnCol, ref int blackPawnRow, int blackPawnCol, int turnCounter)
+        {
+            if (turnCounter % 2 == 0)
+            {
+                board[whitePawnRow--, whitePawnCol] = '-';
+                board[whitePawnRow, whitePawnCol] = 'w';
+
+                if ((whitePawnCol > 0 && board[whitePawnRow, whitePawnCol - 1] == 'b') || (whitePawnCol < BoardSize - 1 && board[whitePawnRow, whitePawnCol + 1] == 'b'))
+                {
+                    return $"Game over! White capture on {(char)(blackPawnCol + 'a')}{BoardSize - blackPawnRow}.";
+                }
+
+                if (whitePawnRow == 0)
+                {
+                    return $"Game over! White pawn is promoted to a queen at {(char)(whitePawnCol + 'a')}{BoardSize - whitePawnRow}.";
+                }
+            }
+            else
+            {
+                board[blackPawnRow++, blackPawnCol] = '-';
+                board[blackPawnRow, blackPawnCol] = 'b';
+
+                if ((blackPawnCol > 0 && board[blackPawnRow, blackPawnCol - 1] == 'w') || (blackPawnCol < BoardSize - 1 && board[blackPawnRow, blackPawnCol + 1] == 'w'))
+                {
+                    return $"Game over! Black capture on {(char)(whitePawnCol + 'a')}{BoardSize - whitePawnRow}.";
+                }
+
+                if (blackPawnRow == BoardSize - 1)
+                {
+                    return $"Game over! Black pawn is promoted to a queen at {(char)(blackPawnCol + 'a')}{BoardSize - blackPawnRow}.";
+                }
+            }
+
+            return null;
+        }
+
+        static char[,] GetBoardData()
+        {
+            char[,] board = new char[BoardSize, BoardSize];
+
+            for (int row = 0; row < BoardSize; row++)
+            {
+                string line = Console.ReadLine();
+
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    board[row, col] = line[col];
+                }
+            }
+
+            return board;
+        }
+
+        static (int, int) GetPawnPosition(char[,] board, char color)
+        {
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    if (board[row, col] == color)
+                    {
+                        return (row, col);
+                    }
+                }
+            }
+
+            return (0, 0);
+        }
+    }
+}
---
+++ CSharp-Advanced-September-2022/Labs-And-Exercises/ExamPreparation2/03.StockMarket/Investor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            this.Portfolio = new List<Stock>();
+            this.FullName = fullName;
+            this.EmailAddress = emailAddress;
+            this.MoneyToInvest = moneyToInvest;
+            this.BrokerName = brokerName;
+        }
+
+        public List<Stock> Portfolio { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => this.Portfolio.Count;
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && this.MoneyToInvest >= stock.PricePerShare)
+            {
+                this.Portfolio.Add(stock);
+                this.MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            Stock stock = FindStock(companyName);
+
+            if (stock == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+            else if (sellPrice < stock.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+            else
+            {
+                this.Portfolio.Remove(stock);
+                this.MoneyToInvest += sellPrice;
+                return $"{companyName} was sold.";
+            }
+        }
+
+        public Stock FindStock(string companyName) => this.Portfolio.Find(s => s.CompanyName == companyName);
+
+        public Stock FindBiggestCompany() => this.Portfolio.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The investor {this.FullName} with a broker {this.BrokerName} has stocks:");
+
+            foreach (var stock in this.Portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
